004744bbd239a0fb5e446f738a531e38fb4dcf7e
Darko Pancev <igededarkopancev@gmail.com>
2022-07-18 15:29:33 +0800

    update indentline

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -5,13 +5,14 @@ end
 
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 vim.g.indent_blankline_filetype_exclude = {
-	"help",
-	"startify",
-	"dashboard",
-	"packer",
-	"neogitstatus",
-	"NvimTree",
-	"Trouble",
+  "help",
+  "startify",
+  "dashboard",
+  "packer",
+  "neogitstatus",
+  "NvimTree",
+  "Trouble",
+  "text"
 }
 vim.g.indentLine_enabled = 1
 -- vim.g.indent_blankline_char = "│"
@@ -20,6 +21,7 @@ vim.g.indent_blankline_char = "▏"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = true
+-- vim.g.indent_blankline_use_treesitter_scope = 1
 vim.g.indent_blankline_show_current_context = true
 vim.g.indent_blankline_context_patterns = {
   "class",
@@ -44,7 +46,7 @@ vim.g.indent_blankline_context_patterns = {
   "operation_type",
 }
 -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
-vim.wo.colorcolumn = "99999"
+-- vim.wo.colorcolumn = "99999"
 
 -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
@@ -57,7 +59,7 @@ vim.wo.colorcolumn = "99999"
 -- vim.opt.listchars:append "space:"
 -- vim.opt.listchars:append "eol:↴"
 
-indent_blankline.setup({
+indent_blankline.setup {
   -- show_end_of_line = true,
   -- space_char_blankline = " ",
   show_current_context = true,
@@ -67,4 +69,4 @@ indent_blankline.setup({
   --   "IndentBlanklineIndent2",
   --   "IndentBlanklineIndent3",
   -- },
-})
+}
