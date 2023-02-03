21ecf43f517d28047b2699462afdb0b1f73ace03
darkopancev <darko.pancev@vertexcom.com>
2023-02-03 15:36:17 +0800

    Update nvim-tree open on setup

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -22,11 +22,11 @@ nvim_tree.setup {
   -- disable_netrw = true,
   -- hijack_netrw = true,
   -- open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
+  -- ignore_ft_on_setup = {
+  --  "startify",
+  --  "dashboard",
+  --  "alpha",
+  -- },
   filters = {
     custom = { ".git" },
     exclude = { ".gitignore" },
