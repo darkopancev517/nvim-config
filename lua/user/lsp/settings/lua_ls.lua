68c7d5bdf186b463d61e4d05c262a5c606cf1f75
darkopancev <igededarkopancev@gmail.com>
2023-04-09 02:26:03 +0800

    Add lua ls settings

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -4,6 +4,12 @@ return {
       diagnostics = {
         globals = { "vim" },
       },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
     },
   },
 }
