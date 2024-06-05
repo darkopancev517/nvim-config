243daefddc32d3099e8984a3c6472adbd14db306
Darko Pancev <igededarkopancev@gmail.com>
2024-06-06 07:24:03 +0800

    Fixed prettier formatting setup

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -11,8 +11,8 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
   debug = false,
   sources = {
-    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.prettier,
+    formatting.black,
     formatting.stylua,
   },
 })
